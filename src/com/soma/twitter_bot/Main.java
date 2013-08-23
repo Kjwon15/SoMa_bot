e19915ef7853a8c3ca168d12c757ce298983b841
kjwon15 <kjwonmail@gmail.com>
2013-08-23 21:16:42 +0900

    Add access keys, consumer keys

--- src/com/soma/twitter_bot/Main.java
+++ src/com/soma/twitter_bot/Main.java
@@ -1,5 +1,10 @@
 package com.soma.twitter_bot;
 
 public class Main {
+	private static final String CONSUMER_KEY = "Go2mnRpzVHYRDB6Qc5PQ";
+	private static final String CONSUMER_SECRET = "cs3SXpx4BGHEIpetDK7rdf8erADDDo9co6628vCjWc";
+	
+	private static final String ACCESS_TOKEN = "1693562306-hkliVnRkpmMeCGFDqvkLZIHd18eKcv9l37KXHIN";
+	private static final String ACCESS_TOKEN_SECRET = "EoXKaaedFLYoGWPrBR26iECA0GbsuPUL19xspAJD4v4";
 
 }
