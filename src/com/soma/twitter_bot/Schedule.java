c4b5934b8ccd2224334296a7ef249a6886213939
kjwon15 <kjwonmail@gmail.com>
2013-08-24 03:44:08 +0900

    add some Advertise text.

--- src/com/soma/twitter_bot/Schedule.java
+++ src/com/soma/twitter_bot/Schedule.java
@@ -26,8 +26,14 @@ public class Schedule {
 	private static class ScheduledAutoTweet extends TimerTask {
 		@Override
 		public void run() {
-			String[] autoTweets = { "우리는 장학금을 원합니다.", "수면실 지금 비었나요?",
-					"으이이 샤워하고싶당", "집에 가고싶어요..ㅠㅠ" };
+			String[] autoTweets = {
+					"우리는 장학금을 원합니다.",
+					"수면실 지금 비었나요?",
+					"으이이 샤워하고싶당",
+					"집에 가고싶어요..ㅠㅠ",
+					"SW_Maestro_ 지원 절차는 다음과 같습니다. http://swmaestro.kr/homepage/default/page/subLocation.do?menu_no=20010301&up_menu_no=200103",
+					"SW_Maestro_ 는 3단계 인증과정을 통해 SW 인재를 양성하는 프로그램입니다.",
+					"SW_Maestro_ 홈페이지는 http://swmaestro.kr/ 이곳입니다.",};
 			tw.tweet(choice(autoTweets));
 		}
 	}
